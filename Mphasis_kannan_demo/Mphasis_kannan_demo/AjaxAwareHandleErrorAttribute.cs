4ec8bc811878b4c25d5633b444bd05a7cd635f30
kannannet <42512797+kannannet@users.noreply.github.com>
2018-10-07 21:51:45 +0530

    Simple add calculator

---
+++ Mphasis_kannan_demo/Mphasis_kannan_demo/AjaxAwareHandleErrorAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mphasis_kannan_demo
+{
+    public class AjaxAwareHandleErrorAttribute : HandleErrorAttribute
+    {
+        public string PartialViewName { get; set; }
+
+        public override void OnException(ExceptionContext filterContext)
+        {
+            base.OnException(filterContext);
+
+            // Verify if AJAX request
+            if (filterContext.HttpContext.Request.IsAjaxRequest())
+            {
+                var result = new HttpStatusCodeResult(410, "exception occured");
+                filterContext.Result = result;
+            }
+        }
+    }
+}
